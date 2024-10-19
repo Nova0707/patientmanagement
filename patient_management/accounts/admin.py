1b9217524bde068ad240070eee9c20b1c474c1dc
aditya <aryan68125@gmail.com>
2024-10-19 15:08:21 +0530

    remove the default code

--- patient_management/accounts/admin.py
+++ patient_management/accounts/admin.py
@@ -1,9 +1,1 @@
-from django.contrib import admin
-from .models import User, Doctor, Receptionist, Patient, TimeSlot, Appointment
 
-admin.site.register(User)
-admin.site.register(Doctor)
-admin.site.register(Receptionist)
-admin.site.register(Patient)
-admin.site.register(TimeSlot)
-admin.site.register(Appointment)
--- patient_management/accounts/urls.py
+++ patient_management/accounts/urls.py
@@ -1,8 +1,4 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('login/', views.login_view, name='login'),
-    path('doctor-dashboard/', views.doctor_dashboard, name='doctor_dashboard'),
-    path('receptionist-dashboard/', views.receptionist_dashboard, name='receptionist_dashboard'),
-]
+urlpatterns = []
