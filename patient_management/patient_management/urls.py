aa606731a58b9e983badbeaec5bd3eec891d064a
aditya <aryan68125@gmail.com>
2024-10-19 19:35:24 +0530

    added receptionist

--- patient_management/patient_management/urls.py
+++ patient_management/patient_management/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
-    path('/receptionist', include('receptionist.urls')),
+    path('receptionist/', include('receptionist.urls')),
 ]
