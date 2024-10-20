bfcdac2d8d0fd8b341cc63d7c97ad40c4bfc2b2d
aditya <aryan68125@gmail.com>
2024-10-20 18:37:21 +0530

    new app doctor created

--- patient_management/receptionist/urls.py
+++ patient_management/receptionist/urls.py
@@ -1,15 +1,5 @@
 from django.urls import path
-#imports related to page render
-from receptionist.views import (
-    BookAppointmentPage,
-    )
-from receptionist.views_bl import (
-    BookAppointmentLogic,
-)
-urlpatterns = [
-    # urls related to page rendering
-    path('',BookAppointmentPage,name="BookAppointmentPage"),
 
-    #urls related ti business logic
-    path('book_appointment/',BookAppointmentLogic.as_view(),name="BookAppointmentLogic"),
+urlpatterns = [
+
 ]
--- patient_management/receptionist/views.py
+++ patient_management/receptionist/views.py
@@ -1,5 +1,3 @@
 from django.shortcuts import render
 
 # render pages
-def BookAppointmentPage(request):
-    return render(request,'receptionists/BookingPage.html')
